daf0d7158b4e12cc967cbc5c7067f20795d416c3
Krishna Khadka <110077078+Krishna-Khadka@users.noreply.github.com>
2024-07-16 19:48:49 +0545

    Footer Section v1 created

--- src/global/Footer.tsx
+++ src/global/Footer.tsx
@@ -4,106 +4,219 @@ import Image from "next/image";
 import Link from "next/link";
 import { FaPhone, FaEnvelope } from "react-icons/fa6";
 import { GrLocation } from "react-icons/gr";
-import Logo from "../../public/logo.png";
+import event1 from "../../public/blog2.jpg";
+import event2 from "../../public/blog3.jpg";
+
+import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa6";
 
 export default function Footer() {
   return (
     <>
-      <div className="bg-[#172f41] pt-24">
+      <div className="footer py-20">
         <div className="container mx-auto">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
-            <div></div>
-            <div className="pl-[2rem] pb-[30px]">
-              <h2 className="footerTitle">Useful link</h2>
-              <ul className="footerList">
-                <li>
-                  <Link href="#" className="">
-                    Application
-                  </Link>
-                </li>
-                <li>
-                  <Link href="#" className="">
-                    FAQ
-                  </Link>
-                </li>
-                <li>
-                  <Link href="#" className="">
-                    Alumni
-                  </Link>
-                </li>
-                <li>
-                  <Link href="#" className="">
-                    Terms & Conditions
-                  </Link>
-                </li>
-                <li>
-                  <Link href="#" className="">
-                    Privacy Policy
-                  </Link>
-                </li>
-              </ul>
+          <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+            {/* footer first div */}
+            <div className="pr-8">
+              <div className="title">
+                <h1>about us</h1>
+              </div>
+              <div>
+                <p className="text-[#b2c0d8] text-base tracking-wide">
+                  Suspendisse non sem ante. Cras pretium gravida leo a
+                  convallis. Nam malesuada interdum metus, sit amet dictum ante
+                  congue eu. Maecenas ut maximus enim.
+                </p>
+              </div>
+              <div className="footer-social mt-6 pb-4">
+                <ul>
+                  <li>
+                    <Link href="/">
+                      <FaFacebookF className="footer-social-icon text-white text-center" />
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/">
+                      <FaInstagram className="footer-social-icon text-white text-center" />
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/">
+                      <FaTwitter className="footer-social-icon text-white text-center" />
+                    </Link>
+                  </li>
+                </ul>
+              </div>
             </div>
-            <div className="pl-[2rem] pb-[30px]">
-              <h2 className="footerTitle">Study</h2>
-              <ul className="footerList">
-                <li>
-                  <Link href="#" className="">
-                    Find a course
-                  </Link>
-                </li>
-                <li>
-                  <Link href="#" className="">
-                    Admissions
-                  </Link>
-                </li>
-                <li>
-                  <Link href="#" className="">
-                    Scholarships
-                  </Link>
-                </li>
-                <li>
-                  <Link href="#" className="">
-                    Study on campus
-                  </Link>
-                </li>
-                <li>
-                  <Link href="#" className="">
-                    Fees adn costs
-                  </Link>
-                </li>
-              </ul>
+
+            {/* footer second div */}
+            <div>
+              <div className="title">
+                <h1>our links</h1>
+              </div>
+              <div className="footer-links flex items-cente  gap-16">
+                <div>
+                  <ul>
+                    <li>
+                      <Link href="/">Home</Link>
+                    </li>
+                    <li>
+                      <Link href="/">About Us</Link>
+                    </li>
+                    <li>
+                      <Link href="/">Programs</Link>
+                    </li>
+                    <li>
+                      <Link href="/">Blog</Link>
+                    </li>
+                    <li>
+                      <Link href="/">Contact Us</Link>
+                    </li>
+                  </ul>
+                </div>
+                <div>
+                  <ul>
+                    <li>
+                      <Link href="/">Home</Link>
+                    </li>
+                    <li>
+                      <Link href="/">About Us</Link>
+                    </li>
+                    <li>
+                      <Link href="/">Programs</Link>
+                    </li>
+                    <li>
+                      <Link href="/">Blog</Link>
+                    </li>
+                    <li>
+                      <Link href="/">Contact Us</Link>
+                    </li>
+                  </ul>
+                </div>
+              </div>
             </div>
-            <div className="pl-[2rem] pb-[30px]">
-              <h2 className="footerTitle">Inquiries</h2>
-              <ul className="footerContact">
-                <li className=" flex gap-2">
-                  <div>
-                    <GrLocation className="footerIcon" />
-                  </div>
-                  <div className="pl-2">
-                    <p>Address</p>
-                    <Link href="#">Sundarharaincha-12, Morang</Link>
-                  </div>
-                </li>
-                <li className="flex gap-2">
-                  <div>
-                    <FaEnvelope className="footerIcon" />
-                  </div>
-                  <div className="pl-2">
-                    <p>Email</p>
-                    <Link href="#">siddhartha123@gmail.com</Link>
-                  </div>
-                </li>
-                <li className="flex gap-2">
-                  <div>
-                    <FaEnvelope className="footerIcon" />
-                  </div>
-                  <div className="pl-2">
-                    <p>Phone</p>
-                    <Link href="#">+977 9852054850</Link>
-                  </div>
-                </li>
-              </ul>
+
+            {/* footer third div */}
+            <div className="pr-8">
+              <div className="title">
+                <h1>latest posts</h1>
+              </div>
+              <div className="footer-event-list">
+                <ul>
+                  <li>
+                    <Link href="/">
+                      <div className="footer-event flex items-center gap-3">
+                        <div
+                          className="footer-event-image"
+                          style={{ width: "100px", height: "80px" }}
+                        >
+                          <Image
+                            src={event1}
+                            alt="Event Banner Image"
+                            className="w-full h-full object-cover rounded-lg"
+                          />
+                        </div>
+                        <div className="footer-event-info">
+                          <h5 className="text-[#fffc] tracking-wider">
+                            Nothing impossble to need hard work
+                          </h5>
+                          <span className="text-[#FF3115] tracking-wider">
+                            7 March, 2023
+                          </span>
+                        </div>
+                      </div>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/">
+                      <div className="footer-event flex items-center gap-3">
+                        <div
+                          className="footer-event-image"
+                          style={{ width: "100px", height: "80px" }}
+                        >
+                          <Image
+                            src={event1}
+                            alt="Event Banner Image"
+                            className="w-full h-full object-cover rounded-lg"
+                          />
+                        </div>
+                        <div className="footer-event-info">
+                          <h5 className="text-[#fffc] tracking-wider">
+                            Nothing impossble to need hard work
+                          </h5>
+                          <span className="text-[#FF3115] tracking-wider">
+                            7 March, 2023
+                          </span>
+                        </div>
+                      </div>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            {/* footer last div */}
+            <div>
+              <div className="title">
+                <h1>contact us</h1>
+              </div>
+              <div className="footer-contact-list">
+                <ul>
+                  <li>
+                    {/* contact phone div */}
+                    <div className="footer-contact flex items-center gap-3">
+                      {/* icon */}
+                      <div className="footer-contact-icon">
+                        <FaPhone />
+                      </div>
+                      {/* info  */}
+                      <div className="flex flex-col">
+                        <Link href="/" className="tracking-wider">
+                          +977 9853214780
+                        </Link>
+                        <Link href="/" className="tracking-wider">
+                          +977 97351478825
+                        </Link>
+                      </div>
+                    </div>
+                  </li>
+                  <li>
+                    {/* contact mail div */}
+                    <div className="footer-contact flex items-center gap-3">
+                      {/* icon */}
+                      <div className="footer-contact-icon">
+                        <FaEnvelope />
+                      </div>
+                      {/* info  */}
+                      <div className="flex flex-col">
+                        <Link href="/" className="tracking-wider">
+                          info@example.com
+                        </Link>
+                        <Link href="/" className="tracking-wider">
+                          info@demo.com
+                        </Link>
+                      </div>
+                    </div>
+                  </li>
+                  <li>
+                    {/* contact address div */}
+                    <div className="footer-contact flex items-center gap-3">
+                      {/* icon */}
+                      <div className="footer-contact-icon">
+                        <GrLocation />
+                      </div>
+                      {/* info  */}
+                      <div className="flex flex-col">
+                        <Link href="/" className="tracking-wider">
+                          Sundarharainch-10
+                        </Link>
+                        <Link href="/" className="tracking-wider">
+                          Khorsane, Morang
+                        </Link>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
